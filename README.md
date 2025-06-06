36db11dab66615532ac26403910885de912daaaa
DavidSolanes <david.solanes@viewnext.com>
2025-06-06 16:25:11 +0200

    Actualizacion codigo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WS_Spring-web
+
+Arrancar los prouyectos en el siguiente orden:
+
+    1.- eureka-server
+    2.- msvc-productos
+    3.- msvc-items
+    4.- msvc-gateway-server
--- msvc-products/src/main/java/com/david/springcloud/msvc/products/controllers/ProductController.java
+++ msvc-products/src/main/java/com/david/springcloud/msvc/products/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.david.springcloud.msvc.products.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.david.springcloud.msvc.products.entities.Product;
 import com.david.springcloud.msvc.products.services.ProductService;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
+/*
+@RequestMapping("/api/products")
+*/
 public class ProductController {
 
     final private ProductService service;
@@ -27,7 +32,15 @@ public class ProductController {
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<Product> details(@PathVariable Long id) {
+    public ResponseEntity<Product> details(@PathVariable Long id) throws InterruptedException {
+
+        if (id.equals(10L)) {
+            throw new IllegalStateException("Producto no encontrado!");
+        }
+        if (id.equals(7L)) {
+            TimeUnit.SECONDS.sleep(3L);
+        }
+
         Optional<Product> productOptional = service.findById(id);
         if (productOptional.isPresent()) {
             return ResponseEntity.ok(productOptional.orElseThrow());
